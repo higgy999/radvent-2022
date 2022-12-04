2dc42bdc567f5d5844685eccfe5bd1a505b3e314
Litl Toast <joshuafhiggins@gmail.com>
2022-12-04 13:18:26 -0500

    part 1 done, starting part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -2,19 +2,47 @@ use std::fs;
 
 fn main() {
     part_one();
+    part_two();
 }
 
 fn part_one() {
     let contents = fs::read_to_string("input.txt")
         .expect("Should have been able to read the file");
 
+    let mut increment: i32 = 0;
     for line in contents.lines() {
         if line != "" {
             let split = line.split(",");
             let vec: Vec<&str> = split.collect();
-            for string in vec {
-                println!("{}", string);
+
+            let vec_of_first_range: Vec<&str> = vec.get(0).unwrap().split("-").collect();
+            let vec_of_second_range: Vec<&str> = vec.get(1).unwrap().split("-").collect();
+
+            let first_in_first = vec_of_first_range.get(0).unwrap().parse::<i32>().unwrap();
+            let second_in_first = vec_of_first_range.get(1).unwrap().parse::<i32>().unwrap();
+            let first_in_second = vec_of_second_range.get(0).unwrap().parse::<i32>().unwrap();
+            let second_second = vec_of_second_range.get(1).unwrap().parse::<i32>().unwrap();
+
+            if first_in_first >= first_in_second && first_in_first <= second_second {
+                if second_in_first >= first_in_second && second_in_first <= second_second {
+                    increment += 1;
+                    println!("{} -> {}", &line, true);
+                    continue;
+                }
             }
+            if first_in_second >= first_in_first && second_second <= second_in_first {
+                if first_in_second >= first_in_first && second_second <= second_in_first {
+                    increment += 1;
+                    println!("{} -> {}", &line, true);
+                    continue;
+                }
+            }
+            println!("{} -> {}", &line, false);
         }
     }
+    println!("In how many assignment pairs does one range fully contain the other? {}", increment);
+}
+
+fn part_two() {
+    
 }
