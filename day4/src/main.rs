e468b12c91ccfd15e21f08ffd7c538ab80547252
Litl Toast <joshuafhiggins@gmail.com>
2022-12-04 12:26:32 -0500

    split at comma per line

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -7,4 +7,14 @@ fn main() {
 fn part_one() {
     let contents = fs::read_to_string("input.txt")
         .expect("Should have been able to read the file");
+
+    for line in contents.lines() {
+        if line != "" {
+            let split = line.split(",");
+            let vec: Vec<&str> = split.collect();
+            for string in vec {
+                println!("{}", string);
+            }
+        }
+    }
 }
